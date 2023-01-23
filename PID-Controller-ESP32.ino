e215202c1337882fc3c6d81ab5ea907e8992ad63
Asanka Lakmal Morawaka <almorawaka@gmail.com>
2023-01-23 22:23:03 +0530

    buzzer, stausLED
    buzzer to generate startup tone, temperature alternation tones, etc. Two LEDs ready(GREEN) notReady (RED) adds to recognize the temperature if not in the range

--- PID-Controller-ESP32.ino
+++ PID-Controller-ESP32.ino
@@ -5,8 +5,8 @@
 #include <PIDController.h>
 #include "max6675.h"
 // Define Rotary Encoder Pins
-#define CLK_PIN 16
-#define DATA_PIN 2
+#define CLK_PIN 4
+#define DATA_PIN 17
 #define SW_PIN 15
 // MAX6675 Pins
 #define thermoDO  14
@@ -26,19 +26,25 @@
 #define __Kp 30 // Proportional constant
 #define __Ki 0.7 // Integral Constant
 #define __Kd 200 // Derivative Constant
-int clockPin; // Placeholder por pin status used by the rotary encoder
+const int buzzer = 27; //buzzer to arduino pin 27
+const int ready = 26;
+const int notReady = 25;
+boolean startup_tone = true;
+int clockPin=0; // Placeholder por pin status used by the rotary encoder
 int clockPinState; // Placeholder por pin status used by the rotary encoder
-int set_temperature = 1; // This set_temperature value will increas or decreas if when the rotarty encoder is turned
+float set_temperature = 30.0; // This set_temperature value will increas or decreas if when the rotarty encoder is turned
 float temperature_value_c = 0.0; // stores temperature value
-long debounce = 0; // Debounce delay
+float temperature_cal_c = 0;  // lenear adjestment of temperature
+unsigned long debounce = 0; // Debounce delay
 int encoder_btn_count = 0; // used to check encoder button press
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO); // Create an instance for the MAX6675 Sensor Called "thermocouple"
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);// Create an instance for the SSD1306 128X64 OLED "display"
 PIDController pid; // Create an instance of the PID controller class, called "pid"
 void setup() {
 #ifdef __DEBUG__
-  Serial.begin(9600);
+  Serial.begin(115200);
 #endif
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 27 as an output
   pinMode(mosfet_pin, OUTPUT); // MOSFET output PIN
   pinMode(CLK_PIN, INPUT); // Encoer Clock Pin
   pinMode(DATA_PIN, INPUT); //Encoder Data Pin
@@ -68,37 +74,57 @@ void setup() {
   display.display(); // Update the Display
   delay(2000); // Delay of 200 ms
 }
-void set_temp()
-{
+void set_temp() {
   if (encoder_btn_count == 2) // check if the button is pressed twice and its in temperature set mode.
   {
     display.clearDisplay(); // clear the display
     display.setTextSize(2); // Set text Size
-    display.setCursor(16, 0); // set the diplay cursor
+    display.setCursor(5, 0); // set the diplay cursor
     display.print("Set Temp."); // Print Set Temp. on the display
-    display.setCursor(45, 25); // set the cursor
+    display.setTextSize(4); // Set text Size
+    display.setCursor(5, 30); // set the cursor
     display.print(set_temperature);// print the set temperature value on the display
     display.display(); // Update the Display
   }
 }
-void read_encoder() // In this function we read the encoder data and increment the counter if its rotaing clockwise and decrement the counter if its rotating counter clockwis
-{
+
+void show_temp(){
+      display.clearDisplay(); // Clear the display
+    display.setTextSize(2); // Set text Size
+    display.setCursor(5, 0); // Set the Display Cursor
+    display.print("Cur Temp."); //Print to the Display
+    display.setCursor(5, 30);// Set the Display Cursor
+    display.setTextSize(4); // Set text Size
+    display.print(temperature_value_c); // Print the Temperature value to the display in celcius
+    display.display(); // Update the Display
+    delay(200); // Wait 200ms to update the OLED dispaly.
+  }
+
+
+void read_encoder(){ // In this function we read the encoder data and increment the counter if its rotaing clockwise and decrement the counter if its rotating counter clockwis
+while (encoder_btn_count==2){
   clockPin = digitalRead(CLK_PIN); // we read the clock pin of the rotary encoder
   if (clockPin != clockPinState  && clockPin == 1) { // if this condition is true then the encoder is rotaing counter clockwise and we decremetn the counter
-    if (digitalRead(DATA_PIN) != clockPin) set_temperature = set_temperature - 3;  // decrmetn the counter.
-    else  set_temperature = set_temperature + 3; // Encoder is rotating CW so increment
-    if (set_temperature < 1 )set_temperature = 1; // if the counter value is less than 1 the set it back to 1
-    if (set_temperature > 150 ) set_temperature = 150; //if the counter value is grater than 150 then set it back to 150 
+    if (digitalRead(DATA_PIN) != clockPin) {
+      set_temperature = set_temperature - 1;// decrmetn the counter.
+      tone2();
+      }  
+    else { 
+      set_temperature = set_temperature + .2;// Encoder is rotating CW so increment
+      tone2();
+      } 
+    if (set_temperature < 20 )set_temperature = 20; // if the counter value is less than 1 the set it back to 1
+    if (set_temperature > 38 ) set_temperature = 38; //if the counter value is grater than 150 then set it back to 150 
 #ifdef __DEBUG__
     Serial.println(set_temperature); // print the set temperature value on the serial monitor window
 #endif
   }
   clockPinState = clockPin; // Remember last CLK_PIN state
-  
   if ( digitalRead(SW_PIN) == LOW)   //If we detect LOW signal, button is pressed
   {
-    if ( millis() - debounce > 80) { //debounce delay
+    if ( millis() - debounce > 60) { //debounce delay
       encoder_btn_count++; // Increment the values 
+      tone2();
       if (encoder_btn_count > 2) encoder_btn_count = 1;
 #ifdef __DEBUG__
       Serial.println(encoder_btn_count);
@@ -106,29 +132,78 @@ void read_encoder() // In this function we read the encoder data and increment t
     }
     debounce = millis(); // update the time variable
   }
+   delay(1);// Put in a slight delay to help debounce the reading
+   set_temp(); // Call the Set Temperature Function
 }
-void loop()
+}
+
+int read_encorder_sw() {
+  clockPinState = clockPin; // Remember last CLK_PIN state
+  if ( digitalRead(SW_PIN) == LOW)   //If we detect LOW signal, button is pressed
+  { if ( millis() - debounce > 60) { //debounce delay
+       encoder_btn_count++; // Increment the values 
+       tone2();
+      if (encoder_btn_count > 2) encoder_btn_count = 1;
+#ifdef __DEBUG__
+      Serial.println(encoder_btn_count);
+#endif
+    }
+    debounce = millis(); // update the time variable
+  }
+  return encoder_btn_count;
+}
+
+void tone1(){  // start up tone
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);        // ...for 1sec
+}
+
+void tone2(){ // tone to recogmize set values changes
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(100);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  //delay(1000);        // ...for 1sec
+}
+
+
+void loop() // Put in a slight delay to help debounce the reading
 {
-  read_encoder(); //Call The Read Encoder Function
-  set_temp(); // Call the Set Temperature Function
-  if (encoder_btn_count == 1) // check if the button is pressed and its in Free Running Mode -- in this mode the arduino continiously updates the screen and adjusts the PWM output according to the temperature.
-  {
+  if(startup_tone){
+      tone1();
+      startup_tone=false;
+    }
+  encoder_btn_count = read_encorder_sw(); //read the encoeder switch value 1 or 2
+
+  while(encoder_btn_count == 2){ 
+   encoder_btn_count = read_encorder_sw(); //read the encoeder switch value 1 or 2
+   analogWrite(mosfet_pin, 0); //  turn off heater untill set the temperature
+   read_encoder(); //Call The Read Encoder Function to set the Temperature
+     }
+  while(encoder_btn_count == 1){ // check if the button is pressed and its in Free Running Mode -- in this mode the arduino continiously updates the screen and adjusts the PWM output according to the temperature.
+    encoder_btn_count = read_encorder_sw(); //read the encoeder switch value 1 or 2
     temperature_value_c = thermocouple.readCelsius(); // Read the Temperature using the readCelsius methode from MAX6675 Library.
+    temperature_value_c =  temperature_value_c +  temperature_cal_c; // linear adjstment of tempeture  
     int output = pid.compute(temperature_value_c);    // Let the PID compute the value, returns the optimal output
     analogWrite(mosfet_pin, output);           // Write the output to the output pin
     pid.setpoint(set_temperature); // Use the setpoint methode of the PID library to
-    display.clearDisplay(); // Clear the display
-    display.setTextSize(2); // Set text Size
-    display.setCursor(16, 0); // Set the Display Cursor
-    display.print("Cur Temp."); //Print to the Display
-    display.setCursor(45, 25);// Set the Display Cursor
-    display.print(temperature_value_c); // Print the Temperature value to the display in celcius
-    display.display(); // Update the Display
-#ifdef __DEBUG__
+    show_temp(); // show the current tem on display
+
+    if(set_temperature-temperature_value_c>1 || temperature_value_c-set_temperature>1 ){
+      digitalWrite(notReady,HIGH);
+      digitalWrite(ready,LOW);
+    }
+
+    if(set_temperature-temperature_value_c<1 || temperature_value_c-set_temperature>1 ){
+      digitalWrite(ready,HIGH);
+      digitalWrite(notReady,LOW);
+    }
+
+    #ifdef __DEBUG__
     Serial.print(temperature_value_c); // Print the Temperature value in *C on serial monitor
     Serial.print("   "); // Print an Empty Space
     Serial.println(output); // Print the Calculate Output value in the serial monitor.
-#endif
-    delay(200); // Wait 200ms to update the OLED dispaly.
+    #endif
   }
 }
